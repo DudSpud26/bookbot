47f7d0ac61c5490f1fe20f66c813bf2f9f39ad5b
Dudspud26 <vernonseager@gmail.com>
2025-03-30 11:37:49 -0400

    added command line argument capability

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_book_text(filepath):
     book_as_string = ""
     with open(filepath) as f:
@@ -18,15 +20,20 @@ def print_character_counts(dict_list):
 
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
     book_as_string = get_book_text(file_path)
     total_word_count = get_word_count(book_as_string)
+    character_count_list = get_sorted_list(get_character_count(book_as_string))
     print("============ BOOKBOT ============")
     print(f"Analyzing book fount at {file_path}...")
     print("----------- Word Count -----------")
     print(f"Found {total_word_count} total words")
     print("--------- Character Count ---------")
-    print_character_counts(get_sorted_list(get_character_count(book_as_string)))
+    print_character_counts(character_count_list)
     print("============ END ============")
   
 main()
