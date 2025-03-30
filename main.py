679beae68ca06c5cc15e2b80fc21a5e8454d4589
Dudspud26 <vernonseager@gmail.com>
2025-03-30 11:23:51 -0400

    added get_sorted_list function and changed main.py to print a nice message

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ def get_book_text(filepath):
 
 from stats import get_word_count 
 
-    
-def main():
-    frankenstein_word_count = get_word_count(get_book_text("books/frankenstein.txt"))
-    print(f"{frankenstein_word_count} words found in the document")
+from stats import get_character_count
 
+from stats import get_sorted_list
+
+def print_character_counts(dict_list):
+    for entry in dict_list:
+        if entry["name"].isalpha():
+            print(f"{entry["name"]}: {entry["num"]}")
+
+
+def main():
+    file_path = "books/frankenstein.txt"
+    book_as_string = get_book_text(file_path)
+    total_word_count = get_word_count(book_as_string)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book fount at {file_path}...")
+    print("----------- Word Count -----------")
+    print(f"Found {total_word_count} total words")
+    print("--------- Character Count ---------")
+    print_character_counts(get_sorted_list(get_character_count(book_as_string)))
+    print("============ END ============")
+  
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,28 @@ def get_word_count(string):
         num_words += 1
     
     return num_words
+
+def get_character_count(string):
+    character_dictionary = {}
+    for character in string:
+        lower_case = character.lower()
+        if lower_case in character_dictionary:
+            character_dictionary[lower_case] += 1
+        else:
+            character_dictionary[lower_case] = 1
+    
+    return character_dictionary
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_list(dict):
+    result = []
+    for entry in dict:
+        item = {"name":entry,"num":dict[entry]}
+        result.append(item)
+    
+    result.sort(reverse=True, key=sort_on)
+    return result
+
+
